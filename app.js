61fe77446e809f2b95078f41925f41dc6879c458
morganthomas <morgan.a.s.thomas@gmail.com>
2015-08-01 15:46:37 -0600

    Starting node branch.

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+var server = app.listen(5051, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
