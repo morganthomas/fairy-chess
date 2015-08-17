2e48b1ace3211bc37c49f5c0d016c83d418fba8a
morganthomas <morgan.a.s.thomas@gmail.com>
2015-08-17 11:13:50 -0600

    Update to test deploy setup.

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(passportConfig.ensureAuthenticated);
 
 app.get('/', indexController.index);
 
-console.log("Test update 4!")
+console.log("Test update 5!")
 
 var server = app.listen(3000, function() {
 	console.log('Express server listening on port ' + server.address().port);
